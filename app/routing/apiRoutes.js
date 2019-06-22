9568422aea6e19b2a18cdfaec81d7c13d12ed8e1
PTMcEneaney <pagemceneaney@gmail.com>
2019-06-22 13:49:12 -0700

    create friend finder logic, update survey post response

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,58 +17,48 @@ router.get('/friends', function(req, res) {
 
 router.post('/friends', function(req, res) {
         var userInput = req.body;
-        console.log(userInput);
 		var userResponses = userInput.scores;
 
-        // friends.push(userInput);
-        res.json(userInput);
-        list.push(userInput);
+        // res.json(userInput);
 
+        // Compute best friend match
+		var matchName = '';
+        var matchImage = '';
+        var matchScores = [];
+		var totalDifference = 10000; // Make the initial value big for comparison
 
-});
-
-// router.post("/friends", function(req, res) {
-//    	// Capture the user input object
-// 		var userInput = req.body;
-		
-
-// 		var userResponses = userInput.scores;
-		
-
-// 		// Compute best friend match
-// 		var matchName = '';
-// 		var matchImage = '';
-// 		var totalDifference = 10000; // Make the initial value big for comparison
-
-// 		// Examine all existing friends in the list
-// 		for (var i = 0; i < friends.length; i++) {
+		// Examine all existing friends in the list
+		for (var i = 0; i < list.length; i++) {
 			
 
-// 			// Compute differenes for each question
-// 			var diff = 0;
-// 			for (var j = 0; j < userResponses.length; j++) {
-// 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-// 			}
+			// Compute differences for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(list[i].scores[j] - userResponses[j]);
+			}
 			
 
-// 			// If lowest difference, record the friend match
-// 			if (diff < totalDifference) {
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
 				
 
-// 				totalDifference = diff;
-// 				matchName = friends[i].name;
-// 				matchImage = friends[i].photo;
-// 			}
-// 		}
+				totalDifference = diff;
+				matchName = list[i].name;
+                matchImage = list[i].photo;
+                matchScores = list[i].scores;
+			}
+		}
 
-// 		// Add new user
-// 		friends.push(userInput);
+        // , matchScores: matchScores
+        console.log(matchScores)
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage, matchScores: matchScores});
 
-// 		// Send appropriate response
-// 		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-// 	});
-// });
-  
+        // Add new user
+        list.push(userInput);
+
+        
+});  
 
   
   
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 var express = require("express");
+// Sets up the Express App
+var app = express();
+
 
 var htmlR = require("./app/routing/htmlRoutes");
 var apiR = require("./app/routing/apiRoutes");
 
-// Sets up the Express App
-var app = express();
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// var mysql = require("mysql");
-
 var PORT = 3000;
 
-app.use("/", htmlR);
+app.use("/", htmlR); 
 app.use("/api", apiR);
 
 
