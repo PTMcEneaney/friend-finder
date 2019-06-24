4ea5ada4f4968df24e2490acf6d153882d860423
PTMcEneaney <pagemceneaney@gmail.com>
2019-06-24 16:16:24 -0700

    heroku push test

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,18 +8,26 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var public = require('path').join(__dirname,'../public');
-app.use(express.static(public));
+// app.use(express.static(path.join(__dirname,'../public')));
+
+
   
   // Define the home page route
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));;
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
   
   // Define the about route
 router.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
+
   });
+
+// router.get('/images/PageMcEneaney.jpg', function(req, res) {
+//     res.send(path.join(__dirname, "../public/assets/images"));
+
+//   });  
+
   
   
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 // Sets up the Express App
 var app = express();
 
-
 var htmlR = require("./app/routing/htmlRoutes");
 var apiR = require("./app/routing/apiRoutes");
 
@@ -15,23 +14,6 @@ var PORT = process.env.PORT || 3000;
 
 app.use("/", htmlR); 
 app.use("/api", apiR);
-
-
-// //home page
-// app.get("/", function(req, res){
-//   res.sendFile(path.join(__dirname, "app/public/home.html"));
-//   console.log("home")
-// });
-// //survey page
-// app.get("/survey", function(req, res){
-//   res.sendFile(path.join(__dirname, "app/public/survey.html"));
-//   console.log("survey")
-// });
-
-//api
-// app.get("/api/friends", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/app/routing/apiRoutes.js"));
-// });
 
   // Starts the server to begin listening
 // =============================================================
